25e4d9566013294d339f15dbf6fc3e983a443a58
Collin Bull <vhiessumaniac@hotmail.com>
2019-04-10 10:28:00 +0100

    .

--- src/components/SongSelector.js
+++ src/components/SongSelector.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const SongSelector = (props) => {
-
   console.log(props.songs);
-    if(props.songs.length === 0) return null;
 
-    const options = props.songs.map((song) => {
-      return <h4>{song['im:artist'].label}</h4>
-    })
+  if(props.songs.length === 0) return null;
+
+  const options = props.songs.map((song) => {
+    return <h4>{song['im:artist'].label}</h4>
+  })
 
   function handleChange(event){
     props.handSelected(event.target.value);
@@ -15,9 +15,9 @@ const SongSelector = (props) => {
 
   return(
     <div>
-     <ol>
-      {options}
-    </ol>
+      <ol>
+        {options}
+      </ol>
     </div>
   );
 
